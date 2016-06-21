81a5b8a286a9c5ababe93e15a7a8a920ea177e2f
Luke Moderwell <lmoderwell@mindboxstudios.com>
2016-06-20 22:51:54 -0400

    makin' bacon
    cooking up some changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 (function(){$(".more").click(function(){$(".footerInfo").toggleClass("closed"),$(".less").toggleClass("hide")}),$(".less").click(function(){$(".footerInfo").toggleClass("closed"),$(".less").toggleClass("hide")})}).call(this);
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
