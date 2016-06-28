40c784c11af2c4ec4a0bfedceff0c95b88d4603b
Luke Moderwell <lmoderwell@mindboxstudios.com>
2016-06-27 22:32:54 -0400

    responsive clean up
    Still can’t get particles.js to work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function(){$(".more").click(function(){$(".footerInfo").toggleClass("closed"),$(".less").toggleClass("hide")}),$(".less").click(function(){$(".footerInfo").toggleClass("closed"),$(".less").toggleClass("hide")})}).call(this);
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
