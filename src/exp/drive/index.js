ecc6276212757d0bf564cbd0087dc715bbaa4c09
Luke Moderwell <luke.moderwell@gmail.com>
2019-03-15 09:06:29 -0400

    add video swipe

--- src/exp/drive/index.js
+++ src/exp/drive/index.js
@@ -1,40 +1,58 @@
-import {
-  TweenMax
-} from "gsap";
+import {TweenMax} from "gsap";
 
 function init() {
   // make sure DOM has loaded, etc
   wait(loadingFadeIn, 500);
   wait(loadingFadeOut, 2000);
   wait(revealVideo, 3000);
-  wait(videoFadeOut, 5000);
+  // wait(videoFadeOut, 5000);
 }
 
 function loadingFadeIn() {
   TweenMax.to('.loading-logo', .75, {
     opacity: 1,
-    y: '0'
+    y: '0',
+    ease: Power1.easeInOut
   });
 }
 
 function loadingFadeOut() {
   TweenMax.to('.loading-logo', .75, {
     opacity: 0,
-    y: '50%',
+    y: '15%',
     ease: Power1.easeInOut
   });
 }
 
 function revealVideo() {
-  TweenMax.to('.video-wrapper', 1.5, {
-    y: 0
+  TweenMax.to('.video-wrapper', .75, {
+    y: 0,
+    ease: Power1.easeInOut
+  });
+
+  // swiper up
+  TweenMax.to('.video-swiper', .75, {
+    height: '100%',
+    ease: Power1.easeInOut
+  });
+
+  TweenMax.to('.video-poster', 0, {
+    opacity: 1,
+    visibility: 'visible',
+    delay: .5
+  });
+
+  // swiper down
+  TweenMax.to('.video-swiper', .75, {
+    height: 0,
+    top: 0,
+    delay: .95,
+    ease: Power1.easeInOut
   });
 }
 
 function videoFadeOut() {
-  TweenMax.to('.video-wrapper', .75, {
-    opacity: .95
-  });
+  TweenMax.to('.video-wrapper', .75, {opacity: .95});
   TweenMax.to('.video-poster', .75, {
     width: 500,
     height: 500,
