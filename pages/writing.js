d12e8470ae99cc83bf8d2812848ea1edbe66b027
Luke Moderwell <luke.moderwell@gmail.com>
2022-01-20 20:31:28 -0500

    adjusting blog settings

--- pages/writing.js
+++ pages/writing.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import Layout from '../src/components/Layout'
+import postData from '../src/util/posts'
 
 export default function Writing({ posts }) {
   return (
-    <Layout frontMatter={{ title: "Selected Writing" }}>
-      {/* {posts.map((post, index) => (
-        <h1 key={index}>Hi There</h1>
-      ))} */}
+    <Layout frontMatter={{ title: 'Selected Writing' }}>
+      {posts.map((post, index) => (
+        <h1 key={index}>{post.title}</h1>
+      ))}
       Here is where the content will go.
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const posts = Array.from(postData.values())
+    .map((post) => post.frontMatter)
+    .sort((a, b) => {
+      return Number(new Date(b.date || '')) - Number(new Date(a.date || ''))
+    })
+    .filter((post) => !post.hidden)
+
+  return {
+    props: { posts },
+  }
 }
---
+++ src/util/posts.js
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import read from 'fs-readdir-recursive'
+import matter from 'gray-matter'
+import path from 'path'
+
+const POSTS_PATH = path.join(process.cwd(), 'content')
+console.log(POSTS_PATH)
+const paths = read(POSTS_PATH)
+
+const postsMap = paths
+  .map((filePath) => {
+    const fullPath = path.join(POSTS_PATH, filePath)
+    const source = fs.readFileSync(fullPath)
+
+    const slug = fullPath.replace(/^.*\/blog\//, '').replace('.mdx', '')
+    const ogSlug = slug.replace(/^\//, '').replace(/\//g, '-') + '.png'
+
+    const { content, data } = matter(source)
+    return {
+      content,
+      frontMatter: {
+        ...data,
+        slug,
+        ogSlug,
+      },
+      path: fullPath,
+    }
+  })
+  .map((entry) => {
+    const {
+      frontMatter: { slug },
+    } = entry
+    return [slug, entry]
+  })
+
+export function postsForCategory(category) {
+  return Array.from(postsMap.values()).filter((post) =>
+    post.frontMatter?.categories?.includes(category)
+  )
+}
+
+export const allCategories = Array.from(postsMap.values())
+
+  .reduce((categories, post) => {
+    return post.frontMatter?.categories
+      ? [...categories, ...post.frontMatter.categories]
+      : categories
+  }, [])
+  .filter((v, i, a) => a.indexOf(v) === i)
+
+export default postsMap
