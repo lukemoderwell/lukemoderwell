fc104b782d6376edb878e3592aba026cd5ebd372
Luke Moderwell <luke.moderwell@gmail.com>
2022-01-28 17:19:45 -0500

    removing some extra react imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import moment from 'moment-timezone'
 import Link from 'next/link'
 import widont from '../util/widont'
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import PlainLink from './PlainLink'
 
 const links = [
