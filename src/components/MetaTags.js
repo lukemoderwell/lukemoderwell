9767f603be6032a405644ccca312d76926953de3
Luke Moderwell <luke.moderwell@gmail.com>
2022-01-28 15:51:47 -0500

    trying to fix meta tags

--- src/components/MetaTags.js
+++ src/components/MetaTags.js
@@ -16,15 +16,10 @@ function Metatags({ title, description }) {
       <meta content={description} name="twitter:description" />
       <meta content={description} property="og:description" />
 
-      <meta content="1200" property="og:image:width" />
-      <meta content="630" property="og:image:height" />
-      <meta content="summary_large_image" name="twitter:card" />
-
       <meta content="en" property="og:locale" />
 
       {/* Favicon */}
       <link href="/favicon.ico" rel="icon" type="image/svg+xml" />
-      <link color="#888" href="/images/favicon.svg" rel="mask-icon"></link>
     </Head>
   )
 }
